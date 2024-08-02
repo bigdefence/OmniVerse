70bcb96fd0872a8f608763905ae7d3f41c06af1f
bigdefence <wjdrkdqls12@naver.com>
2024-08-02 21:46:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ lottie_json = load_lottie_url(lottie_url)
 st.markdown("""
 <style>
     [data-testid=stSidebar] {
-        background-image: linear-gradient(135deg, rgba(236,188,188,1) 11%, rgba(148,160,233,1) 86%);
+        background-image: linear-gradient(135deg, rgba(221,77,21,1) 11%, rgba(218,148,233,1) 86%);
     }
     .sidebar-title {
         font-size: 30px !important;
