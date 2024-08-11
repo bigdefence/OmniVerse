7623894f741ac2ef1cf644f30f6926f68cbab9d8
bigdefence <wjdrkdqls12@naver.com>
2024-08-11 23:59:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -377,7 +377,13 @@ def fashion(image, gemini_model,huggingface_api):
     headers = {"Authorization": f"Bearer {huggingface_api}"}
 
     try:
-        response = requests.post(API_URL, headers=headers, json={"inputs": gemini_description})
+        payload = {
+            "inputs": 'A highly detailed and photorealistic image of ' + gemini_description,
+            "negative_prompt": "cartoonish, low quality, blurry, unrealistic, abstract, fantasy",
+            "num_inference_steps": 100,  # 이미지 품질 향상을 위해 단계 수를 늘림
+            "guidance_scale": 7.5  # 원하는 스타일을 강화
+        }
+        response = requests.post(API_URL, headers=headers, json=payload)
         response.raise_for_status()  # Raise an exception for bad status codes
         image_bytes = response.content
     except requests.exceptions.RequestException as e:
